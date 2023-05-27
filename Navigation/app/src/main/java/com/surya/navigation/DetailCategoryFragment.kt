8f2f50ac1d81e152260f94f983f38055a1767594
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-28 00:30:52 +0700

    Last add animation

--- Navigation/app/src/main/java/com/surya/navigation/DetailCategoryFragment.kt
+++ Navigation/app/src/main/java/com/surya/navigation/DetailCategoryFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.surya.navigation.databinding.FragmentCategoryBinding
 import com.surya.navigation.databinding.FragmentDetailCategoryBinding
 
@@ -42,6 +43,9 @@ class DetailCategoryFragment : Fragment() {
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
 
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
