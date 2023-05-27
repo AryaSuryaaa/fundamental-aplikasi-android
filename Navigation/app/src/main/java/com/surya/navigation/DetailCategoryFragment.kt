9423131d0c60b383f2b1ea375f58aeed3b6b3dbd
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-27 17:26:05 +0700

    Add DetailCategoryFragment.kt

--- Navigation/app/src/main/java/com/surya/navigation/DetailCategoryFragment.kt
+++ Navigation/app/src/main/java/com/surya/navigation/DetailCategoryFragment.kt
@@ -1,29 +1,27 @@
 package com.surya.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import com.surya.navigation.databinding.FragmentCategoryBinding
+import com.surya.navigation.databinding.FragmentDetailCategoryBinding
 
-class CategoryFragment : Fragment() {
 
-    private var _binding: FragmentCategoryBinding? = null
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
     private val binding get() = _binding!!
-
-    companion object {
-        val EXTRA_NAME = "extra_name"
-        val EXTRA_STOCK = "extra_stock"
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -31,13 +29,12 @@ class CategoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategoryBundle.setOnClickListener{ view ->
-            val mBundle = Bundle()
-            mBundle.putString(EXTRA_NAME, "Lifestyle")
-            mBundle.putLong(EXTRA_STOCK, 7)
-            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
 
-        }
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+        Log.d("getLong", dataDescription.toString())
     }
 
     override fun onDestroy() {
