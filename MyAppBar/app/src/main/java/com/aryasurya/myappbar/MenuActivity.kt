88d2b75af55c5982b09a052780f662474c42b675
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-28 10:34:13 +0700

    Add function pada search bar

--- MyAppBar/app/src/main/java/com/aryasurya/myappbar/MenuActivity.kt
+++ MyAppBar/app/src/main/java/com/aryasurya/myappbar/MenuActivity.kt
@@ -1,11 +1,43 @@
 package com.aryasurya.myappbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.aryasurya.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Memasukkan option meu kw SearchBar
+        binding.searchBar.inflateMenu(R.menu.option_menu)
+        binding.searchBar.setOnMenuItemClickListener{ menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu1-> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.text = searchView.text
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+
     }
 }
