2bf983c7f1c5e0ec358597fa99b9ac9d90e10b3f
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-29 00:00:43 +0700

    update function show fragment and data count

--- TabLayout/app/src/main/java/com/aryasurya/tablayout/SectionsPagerAdapter.kt
+++ TabLayout/app/src/main/java/com/aryasurya/tablayout/SectionsPagerAdapter.kt
@@ -11,17 +11,26 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity){
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
+
+    // untuk menampilkan fragment dan jumlah data
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when(position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
+//        var fragment: Fragment? = null
+//        when(position) {
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+//        return fragment as Fragment
+
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
         }
-        return fragment as Fragment
+        return fragment
     }
+
 
 
 }
