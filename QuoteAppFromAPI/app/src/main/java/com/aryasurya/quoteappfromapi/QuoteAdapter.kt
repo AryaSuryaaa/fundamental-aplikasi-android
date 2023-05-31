452d5d9db26df932f39b2403e766f9e861370164
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-31 17:36:59 +0700

    Add adapter untuk RecyclerView

--- QuoteAppFromAPI/app/src/main/java/com/aryasurya/quoteappfromapi/QuoteAdapter.kt
+++ QuoteAppFromAPI/app/src/main/java/com/aryasurya/quoteappfromapi/QuoteAdapter.kt
@@ -3,7 +3,6 @@ package com.aryasurya.quoteappfromapi
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
