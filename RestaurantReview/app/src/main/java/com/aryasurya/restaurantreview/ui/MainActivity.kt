1bb72ebbc00ae4a2b795e4046cde02d8bd4cfab9
AryaSuryaa <suryasaputra914@gmail.com>
2023-06-01 09:26:41 +0700

    Move MainActivity

--- RestaurantReview/app/src/main/java/com/aryasurya/restaurantreview/ui/MainActivity.kt
+++ RestaurantReview/app/src/main/java/com/aryasurya/restaurantreview/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.aryasurya.restaurantreview
+package com.aryasurya.restaurantreview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aryasurya.restaurantreview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
