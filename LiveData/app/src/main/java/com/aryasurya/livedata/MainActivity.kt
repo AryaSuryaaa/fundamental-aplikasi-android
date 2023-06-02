91e7693dcd8ef65f2321378e0bc737a0ecf93ca4
AryaSuryaa <suryasaputra914@gmail.com>
2023-06-02 15:33:16 +0700

    add library and logic at MainActivity

--- LiveData/app/src/main/java/com/aryasurya/livedata/MainActivity.kt
+++ LiveData/app/src/main/java/com/aryasurya/livedata/MainActivity.kt
@@ -2,10 +2,30 @@ package com.aryasurya.livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.aryasurya.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val liveDataTimerViewModel: MainViewModel by viewModels()
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+//        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        suscribe()
+    }
+
+    private fun suscribe() {
+        val elapsedTimeObserver = Observer<Long?> { along ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, along)
+            activityMainBinding.tvTimer.text = newText
+        }
+
+        liveDataTimerViewModel.getElpasedTime().observe(this, elapsedTimeObserver)
     }
 }
